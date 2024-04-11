8d89816a379d519590234316904679cda0ff62ff
Fudmottin <fudmottin@gmail.com>
2024-04-11 04:50:51 -0400

    Added Miller_Rabin submodule

--- primeFactorization.cpp
+++ primeFactorization.cpp
@@ -1,60 +1,13 @@
 #include <iostream>
-#include <random>
 #include <boost/multiprecision/cpp_int.hpp>
-#include <boost/random.hpp>
+#include "Miller_Rabin/miller_rabin.hpp>"
 
-using namespace boost::multiprecision;
-
-const int MILLERRABINITERATIONS = 40;
-
-cpp_int powMod(cpp_int base, cpp_int exp, const cpp_int& mod) {
-    cpp_int result = 1;
-    base %= mod;
-    while (exp > 0) {
-        if (exp & 1) {
-            result = (result * base) % mod;
-        }
-        base = (base * base) % mod;
-        exp >>= 1;
-    }
-    return result;
-}
-
-bool millerRabinTest(const cpp_int& n) {
-    if (n <= 1 || n == 4) return false;
-    if (n <= 5) return true;
-
-    cpp_int d = n - 1;
-    while ((d & 1) == 0) {
-        d >>= 1;
-    }
-
-    boost::random::mt19937_64 rng(std::random_device{}());
-    for (int i = 0; i < MILLERRABINITERATIONS; i++) {
-        cpp_int a = boost::random::uniform_int_distribution<cpp_int>(2, n - 2)(rng);
-        cpp_int x = powMod(a, d, n);
-        if (x == 1 || x == n - 1) continue;
-
-        bool isPrime = false;
-        for (cpp_int r = 1; r < d; r *= 2) {
-            x = powMod(x, 2, n);
-            if (x == 1) return false;
-            if (x == n - 1) {
-                isPrime = true;
-                break;
-            }
-        }
-
-        if (!isPrime) return false;
-    }
-
-    return true;
-}
+using namespace boost::multiprecision::cpp_int;
 
 cpp_int findFactor(const cpp_int& n) {
     if ((n & 1) == 0)
         return cpp_int(2);
-    if (millerRabinTest(n))
+    if (fudmottin::millerRabinTest(n))
         return n;
     for (cpp_int factor = 3; factor * factor <= n; factor += 2)
         if (n % factor == 0)
@@ -82,7 +35,7 @@ void primeFactorization(cpp_int n) {
 
         if (exp > 1) {
             std::cout << "^";
-            if (!millerRabinTest(exp)) {
+            if (!fudmottin::millerRabinTest(exp)) {
                 std::cout << "(";
                 primeFactorization(exp);
                 std::cout << ")";
